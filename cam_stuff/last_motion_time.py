252c9123f3add3581a21b64ab910649a5363cbc5
Levon Dovlatyan <levondov@berkeley.edu>
2015-03-24 18:30:56 -0700

    test

--- cam_stuff/last_motion_time.py
+++
@@ -1,7 +0,0 @@
-import time
-
-# save requests/time/client_address to text file
-curr_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-request_file = open('last_motion_time.txt','a')
-request_file.write('\n' + curr_time)
-request_file.close()
